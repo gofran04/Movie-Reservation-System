179fcf0fba0b5b7b21199190fa2b2a8c7cb81aa6
gofran04 <docreader04@gmail.com>
2026-01-20 17:46:47 +0200

    install spatie package for Authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, SoftDeletes, HasFactory, Notifiable;
+    use HasApiTokens, SoftDeletes, HasRoles, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.

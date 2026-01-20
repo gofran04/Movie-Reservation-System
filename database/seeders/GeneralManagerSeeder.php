8f36074e50835c27baffcce9d119987b36122f0c
gofran04 <docreader04@gmail.com>
2026-01-20 18:49:01 +0200

    create General-Manager role in a DB sedder,who has All permissions

---
+++ database/seeders/GeneralManagerSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class GeneralManagerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $general_manager_attributes = [
+            'name'              => "TheGeneralManager",
+            'phone'             => '011111111',
+            'email'             => "GeneralManager@mail.com",
+            'password'          => Hash::make('admin123'),
+            'role'              => 'admin',
+
+        ];
+
+        $admin = User::create($general_manager_attributes);
+        $admin_role = Role::create(['guard_name' => 'api','name' => 'General-Manager']);
+        $admin->assignRole($admin_role);
+
+    }
+}

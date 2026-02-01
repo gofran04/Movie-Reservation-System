83e86f388e3eff1f22df431863fb3f9823a14074
gofran04 <docreader04@gmail.com>
2026-02-01 23:43:41 +0200

    build store() to create new showtime without overlaping

---
+++ app/Http/Controllers/API/ShowtimeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Showtime\StoreShowtimeRequest;
+use App\Http\Requests\Showtime\UpdateShowtimeRequest;
+use App\Http\Resources\ShowtimeResource;
+use App\Models\Showtime;
+use App\Models\Movie;
+use Carbon\Carbon;
+
+class ShowtimeController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreShowtimeRequest $request)
+    {
+        $inputs = $request->validated();
+
+        $movie = Movie::findOrFail($inputs['movie_id']);
+        $inputs['end_time'] = Carbon::parse($inputs['start_time'])->addMinutes($movie->duration_minutes)->format('Y-m-d H:i:s');
+
+        $showtime = Showtime::create($inputs);
+
+        return new ShowtimeResource($showtime);
+    }
+
+    public function show(Showtime $showtime)
+    {
+        //
+    }
+
+    public function update(UpdateShowtimeRequest $request, Showtime $showtime)
+    {
+        //
+    }
+
+    public function destroy(Showtime $showtime)
+    {
+        //
+    }
+}

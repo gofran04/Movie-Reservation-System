222ca4fae1d3ab08636d3aa13cb1193a8f8d6554
gofran04 <docreader04@gmail.com>
2026-07-08 22:07:33 +0400

    Edited worker-entrypoint.sh, so it will really check if (cache) table created, and edited entrypoint.sh by remove the (OR) that was exite if th migration failed, it was for debug

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-
 set -e
 
 echo "Starting Laravel container..."
 
-php artisan migrate --force || echo "Migration failed"
-echo "Container kept alive for debugging."
+php artisan migrate --force
 
-# Create symbolic link for storage , (|| true) to ignore if it already exists
 php artisan storage:link || true
 
 # start PHP-FPM (IMPORTANT FINAL STEP, this exec$@ will becomes "exec php-fpm" )

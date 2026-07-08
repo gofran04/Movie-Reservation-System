af4d3c2e301466830955aad0e2caa48275f15299
gofran04 <docreader04@gmail.com>
2026-07-08 21:57:55 +0400

    Edited worker-entrypoint.sh, so it will really check if (cache) table created

--- docker/php/worker-entrypoint.sh
+++ docker/php/worker-entrypoint.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "Waiting for cache table..."
 
-until php artisan tinker --execute="DB::select('select 1 from cache limit 1');" >/dev/null 2>&1
+until php artisan tinker --execute="exit(\Illuminate\Support\Facades\Schema::hasTable('cache') ? 0 : 1);" >/dev/null 2>&1
 do
     sleep 2
 done
 
-echo "Database ready."
+echo "Cache table exists."
 
 exec php artisan queue:work redis --sleep=3 --tries=3

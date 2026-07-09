6790cf14a2a25ba6f1e63c4bcced89e5c72581ff
gofran04 <docreader04@gmail.com>
2026-07-09 21:26:46 +0400

    Run migrations and start Laravel server from entrypoint for Render deployment

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -7,5 +7,7 @@ php artisan migrate --force
 
 php artisan storage:link || true
 
-# start PHP-FPM (IMPORTANT FINAL STEP, this exec$@ will becomes "exec php-fpm" )
-exec "$@"
+# comment :start PHP-FPM (IMPORTANT FINAL STEP, this exec$@ will becomes "exec php-fpm" ), we need this only for local development, in production we will use: exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}, to start laravel's HTTP server to serve the application, while in local we use php-fpm to serve the application through nginx, so we need to comment this line in production, and uncomment the next line to start laravel's HTTP server
+#exec "$@"
+
+exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}

705a60317a0f1f856cc62e2b02c9e7e0b3729a0c
gofran04 <docreader04@gmail.com>
2026-07-13 21:53:43 +0400

    Add seeding to entrypoint.sh after debuging finished

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -5,6 +5,8 @@ echo "Starting Laravel container..."
 
 php artisan migrate --force
 
+php artisan db:seed --force
+
 php artisan storage:link || true
 
 # comment :start PHP-FPM (IMPORTANT FINAL STEP, this exec$@ will becomes "exec php-fpm" ), we need this only for local development, in production we will use: exec php artisan serve --host=0.0.0.0 --port=${PORT:-8000}, to start laravel's HTTP server to serve the application, while in local we use php-fpm to serve the application through nginx, so we need to comment this line in production, and uncomment the next line to start laravel's HTTP server
